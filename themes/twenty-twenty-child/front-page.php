9ff25b7b6712314a781bba9e9df4bb27c9949293
Mithun Nath <mithun.nath@ecompliance.com>
2020-01-04 20:01:38 -0500

    home page sections done

--- themes/twenty-twenty-child/front-page.php
+++ themes/twenty-twenty-child/front-page.php
@@ -1,4 +1,5 @@
-<?php 
+<?php
+
 /**
  * 
  * Template Name: Front Page
@@ -6,10 +7,27 @@
  */
 
 ?>
-<?php get_header(); // get header assets ?>
-<div class="jumbotron">
-    <div class="container">
-        <h1>Test</h1>
+<?php get_header(); // get header assets 
+?>
+<div class="home-page-content">
+    <div class="jumbotron full-screen xyz-data-analysis">
+        <div class="container">
+            <h2>Data Analysis</h2>
+
+        </div>
+    </div>
+    <div class="jumbotron full-screen xyz-asset-management">
+        <div class="container">
+            <h2>Assets Management</h2>
+
+        </div>
+    </div>
+    <div class="jumbotron full-screen xyz-knowledge">
+        <div class="container">
+            <h2>Knowledge</h2>
+
+        </div>
     </div>
 </div>
-<?php get_footer(); // get footer assets ?>
+<?php get_footer(); // get footer assets 
+?>
