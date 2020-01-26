8b60dfb4111f1be8d884b80e685b8ac8e5b37542
Mithun Nath <mithunnath@Mithuns-MacBook-Air.local>
2020-01-26 16:59:56 -0500

    added sample js file

--- themes/twenty-twenty-child/functions.php
+++ themes/twenty-twenty-child/functions.php
@@ -11,7 +11,7 @@ function enqueue_styles() {
     // TODO Combine all these js files by adding them to the src directory later
     
     wp_enqueue_script( 'myequity-scripts', get_stylesheet_directory_uri() . '/js/theme.js', array(), '', true );
-    
+    wp_enqueue_script( 'sup-scripts', get_stylesheet_directory_uri() . '/js/sup.js', array(), '', true );    
     }
     
     add_action( 'wp_enqueue_scripts', 'enqueue_styles' ); // Enque Styles and Scripts
