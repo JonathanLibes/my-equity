fb8e9116e97ee8bef58345204e6c70ee9e34bb75
Mithun Nath <mithun.nath@ecompliance.com>
2020-01-26 17:10:45 -0500

    contact pages

---
+++ themes/twenty-twenty-child/contact.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * 
+ * Template Name: Contact Page
+ * 
+ */
+
+?>
+<?php get_header(); // get header assets 
+?>
+
+<div class="container">
+    <div class="row">
+        <div class="col-lg-6"></div>
+        <div class="col-lg-6"></div>
+        <div class="col-lg-6"></div>
+        <div class="col-lg-6"></div>
+        <div class="col-lg-12"></div>
+    </div>
+</div>
+
+
+<?php get_footer(); // get footer assets 
+?>
