cc2851c6494bbd5f92a30764873965821041426d
Mithun Nath <mithun.nath@ecompliance.com>
2020-01-26 20:09:32 -0500

    contact page

--- themes/twenty-twenty-child/contact.php
+++ themes/twenty-twenty-child/contact.php
@@ -6,20 +6,28 @@
  * 
  */
 
-?>
-<?php get_header(); // get header assets 
+get_header(); // get header assets 
+
+$background_desktop = get_field("background_image_desktop")["sizes"]["large"];
+$background_mobile = get_field("background_image_mobile")["sizes"]["large"];
+$bird_image = get_field("bird_image")["sizes"]["large"];
+$contact_title = get_field("contact_title");
+$contact_subtitle = get_field("contact_subtitle");
+$register = get_field("register_button");
 ?>
 
-<div class="container">
-    <div class="row">
-        <div class="col-lg-6"></div>
-        <div class="col-lg-6"></div>
-        <div class="col-lg-6"></div>
-        <div class="col-lg-6"></div>
-        <div class="col-lg-12"></div>
-    </div>
+<div class="jumbotron contact-page-hero m-hide" style="background-image: url(<?= $background_desktop ?>)">
+    <img class="bird-image" src="<?= $bird_image ?>" alt="Bird">
 </div>
+<div class="jumbotron contact-page-hero d-hide" style="background-image: url(<?= $background_mobile ?>)">
+</div>
+<div class="contact-page-form">
+    <h2 class="title"><?= $contact_title ?></h2>
+    <h3 class="subtitle"><?= $contact_subtitle ?></h3>
+    <?php echo do_shortcode('[contact-form-7 id="223"]'); ?>
 
+    <a href="<?= $register["link"] ?>" class="register-seminar"><?= $register["text"] ?></a>
+</div>
 
 <?php get_footer(); // get footer assets 
 ?>
