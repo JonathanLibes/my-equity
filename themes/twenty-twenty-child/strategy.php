d924b1c82b65503c68429a237ce011ffc427edc1
Mithun Nath <mithun.nath@ecompliance.com>
2020-02-05 01:57:37 -0500

    circles added

--- themes/twenty-twenty-child/strategy.php
+++ themes/twenty-twenty-child/strategy.php
@@ -9,6 +9,7 @@
 $housing_merket = get_field("housing_market");
 $family_rentals = get_field("single_family_rentals");
 $new_construction = get_field("new_construction");
+$investment_strategy = get_field("investment_strategy");
 ?>
 <?php get_header(); ?>
 
@@ -123,40 +124,47 @@ $new_construction = get_field("new_construction");
 <div class="jumbotron housing-market">
     <div class="container-fluid">
         <div class="row">
-            <?php foreach ($housing_merket["circles"] as $key => $circle) : ?>
+            <div class="col-lg-12">
+                <h3><?= $investment_strategy["title"] ?></h3>
+            </div>
+        </div>
+        <div class="row">
+            <?php foreach ($investment_strategy["circles_row_one"] as $key => $circle_one) : ?>
 
                 <div class="col-lg-4 circle-container">
-                    <img class="circle-icon" src="<?= $circle["icon"]["url"] ?>" alt="<?= $circle["icon"]["alt"] ?>" />
+                    <img class="circle-icon" src="<?= $circle_one["icon"] ?>" />
 
-                    <div class="circle" style="background-color: <?= $circle["circle_color"] ?>">
-                        <div class="triangle" style="border-top-color: <?= $circle["triangle_color"] ?>"></div>
+                    <div class="circle" style="background-color: <?= $circle_one["circle_color"] ?>">
+                        <div class="triangle" style="border-top-color: <?= $circle_one["triangle_color"] ?>"></div>
                     </div>
 
 
                 </div>
 
             <?php endforeach; ?>
-            <?php foreach ($housing_merket["circles"] as $key => $circleLabel) : ?>
+            <?php foreach ($investment_strategy["circles_row_one"] as $key => $circleLabel) : ?>
                 <div class="col-lg-4">
                     <p class="label"><?= $circleLabel["label"] ?></p>
                 </div>
             <?php endforeach; ?>
         </div>
+        <br />
+        <br />
         <div class="row">
-            <?php foreach ($housing_merket["circles"] as $key => $circle) : ?>
+            <?php foreach ($investment_strategy["circles_row_two"] as $key => $circle_two) : ?>
 
                 <div class="col-lg-4 circle-container">
-                    <img class="circle-icon" src="<?= $circle["icon"]["url"] ?>" alt="<?= $circle["icon"]["alt"] ?>" />
+                    <img class="circle-icon" src="<?= $circle_two["icon"] ?>"/>
 
-                    <div class="circle" style="background-color: <?= $circle["circle_color"] ?>">
-                        <div class="triangle" style="border-top-color: <?= $circle["triangle_color"] ?>"></div>
+                    <div class="circle" style="background-color: <?= $circle_two["circle_color"] ?>">
+                        <div class="triangle" style="border-top-color: <?= $circle_two["triangle_color"] ?>"></div>
                     </div>
 
 
                 </div>
 
             <?php endforeach; ?>
-            <?php foreach ($housing_merket["circles"] as $key => $circleLabel) : ?>
+            <?php foreach ($investment_strategy["circles_row_two"] as $key => $circleLabel) : ?>
                 <div class="col-lg-4">
                     <p class="label"><?= $circleLabel["label"] ?></p>
                 </div>
