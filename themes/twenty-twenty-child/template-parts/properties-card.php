1328829e4fa698b7fd69f2220f7962db95422833
Mithun Nath <mithun.nath@ecompliance.com>
2020-02-28 01:08:13 -0500

    properties card design

--- themes/twenty-twenty-child/template-parts/properties-card.php
+++ themes/twenty-twenty-child/template-parts/properties-card.php
@@ -1,10 +1,47 @@
-<div class="card properties-card">
-    <div class="card-body">
-        <h4>Testing...</h4>
-
+    <div class="card property-card">
+        <div class="card-header" style="background-image: url('https://assets.wordpress.envato-static.com/uploads/2017/08/tropical-PS53BSA.jpg');">
+        </div>
+        <div class="card-body">
+            <div class="row card-details">
+                <div class="col-6 purchase-price">
+                    <span>Purchase Price</span>
+                    <h5>$250,000</h5>
+                </div>
+                <div class="col-6 down-payment">
+                    <span>Down Payment</span>
+                    <h5>$50,000</h5>
+                </div>
+            </div>
+            <div class="row card-details">
+                <div class="col-4 size">
+                    <span>Size</span>
+                    <h6>3,200 sqft</h6>
+                </div>
+                <div class="col-4 bedrooms">
+                    <span>Bedrooms</span>
+                    <h6>5</h6>
+                </div>
+                <div class="col-4 bathrooms">
+                    <span>Bathrooms</span>
+                    <h6>2.5</h6>
+                </div>
+            </div>
+            <div class="row card-details">
+                <div class="col-4 model">
+                    <span>Model</span>
+                    <h6>Providence</h6>
+                </div>
+                <div class="col-4 metro">
+                    <span>Metro</span>
+                    <h6>Test</h6>
+                </div>
+                <div class="col-4 state">
+                    <span>State</span>
+                    <h6>Florida</h6>
+                </div>
+            </div>
+        </div>
+        <div class="card-footer">
+            <a href="#">I'm Interested - Tell Me More</a>
+        </div>
     </div>
-    <div class="card-footer">
-        <h4>Testing...</h4>
-
-    </div>
-</div>
